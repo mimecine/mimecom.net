07f937103f262671054a2644dfa77b78894b0cf5
Marco Romeny <marco@mimecom.net>
2025-11-16 14:37:33 +0000

    feat: Add Header and Footer components with styling
    - Implemented Footer component displaying current year and author name.
    - Created Header component with site logo and navigation links.
    - Integrated Tailwind CSS for styling in both components.
    - Added global CSS file for theme and font imports.
    - Configured wrangler.jsonc for project settings and asset management.
    - Design and content.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import alpinejs from "@astrojs/alpinejs";
+import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [alpinejs()],
+  adapter: cloudflare(),
+});
